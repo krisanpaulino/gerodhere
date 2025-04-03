346bbf2f16399596bb60dc18c8b22ed65f0de849
krisanpaulino <36087959+krisanpaulino@users.noreply.github.com>
2025-04-03 12:41:04 +0800

    Update frontend.blade.php

--- resources/views/template/frontend.blade.php
+++ resources/views/template/frontend.blade.php
@@ -9,7 +9,7 @@
     <meta name="keywords" content="Fastkart">
     <meta name="author" content="Fastkart">
     <link rel="icon" href="{{ asset('front') }}/assets/images/favicon/2.png" type="image/x-icon">
-    <title>Gerodhere</title>
+    <title>Gerodhere|</title>
 
     <!-- Google font -->
     <link rel="preconnect" href="https://fonts.gstatic.com">
