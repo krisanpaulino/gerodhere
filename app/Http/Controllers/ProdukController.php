4b11ac1fb1e8dc257905d8941f55213ee80c011c
krisanpaulino <36087959+krisanpaulino@users.noreply.github.com>
2025-04-03 22:20:35 +0800

    Update ProdukController.php

--- app/Http/Controllers/ProdukController.php
+++ app/Http/Controllers/ProdukController.php
@@ -84,6 +84,7 @@ class ProdukController extends Controller
             'penenun_id' => 'required',
             'gambar_produk' => 'required',
         ]);
+        dd($request->hasFile('gambar_produk'));
         $path = $request->file('gambar_produk')->storePublicly('produk', 'public');
         $validated['gambar_produk'] = $path;
 
